df53645c84015e456d861844a0637d2ce4f9a5cb
Sangeeta Devi <sd705194@gmail.com>
2023-07-14 15:13:03 +0530

    first commit

--- src/weatherapp.js
+++ src/weatherapp.js
@@ -5,39 +5,57 @@ import DotLoader from "react-spinners/DotLoader";
 export default function Weatherapp() {
 const [city,setcity]=useState("");
 const [Apidata,setApidata]=useState(null);
-const [loadData,setloadData]=useState(true);
+const [loadData,setloadData]=useState(false);
   var url="https://api.openweathermap.org/data/2.5/weather?";
   var appid="0518600400a7c09fc0275e93afb2230f";
-    useEffect(()=>{
-    const Weatherdata=axios.get(`${url}q=${city}&appid=${appid}&units=metric`);   
-    console.log(Weatherdata)
   
-    Weatherdata.then(data=>{setApidata(data)
-      setloadData(false)
+    useEffect(()=>{
+      var Weatherdata;
+      if(city!=="")
+      {       setloadData(true);
+             Weatherdata=axios.get(`${url}q=${city}&appid=${appid}&units=metric`); 
+         Weatherdata.then(data=>{setApidata(data)
+      setloadData(false);
     }).catch(err=>{setApidata(null) 
       setloadData(false)});
+      }
+    
+   
   },[city]);
 
   function cityName()
   {   
   const inputCity=document.querySelector(".inputCity");
-    setcity(inputCity.value);    
+   var inputValue=inputCity.value;
+ 
+    var regex=/^[A-Za-z]+[" "]*[A-Za-z]*$/;
+    if(regex.test(inputValue))
+    {  
+      console.log(inputValue);
+      setcity(inputCity.value);
+    }
+    else
+    {
+    setcity("");
+    setApidata("")
+    }
   }
+  const iconUrl=(icon)=>`https://openweathermap.org/img/wn/${icon}@2x.png`
+  
   return(
     <>
     <div className="container">
       
       <div className="row my-5">
-        <div className="col-md-4 m-auto border text-center mt-4">
-      <p className="mt-4 display-6 "><b> Weather App</b></p> 
+        <div className="col-md-4 m-auto border text-center mt-4 Weather">
+      <p className="mt-4 display-6"><b> Weather App</b></p> 
         <input className="form-control inputCity" type="text" placeholder="Enter city name here" aria-label="default input example"/>
        
-         <button type="button" className="btn btn-success w-50 mt-2"  onClick={cityName}>Search</button>              
+         <button type="button" className="btn btn-danger w-50 mt-2"  onClick={cityName}>Search</button>              
          {
         loadData?<DotLoader
         color="blue"
         loading={loadData}
-        // cssOverride={override}
         size={50}
         aria-label="Loading Spinner"
         data-testid="loader"
@@ -51,6 +69,8 @@ const [loadData,setloadData]=useState(true);
            </div>
            <div className="temp">
            Temperature-{Apidata && Apidata.data.main.temp}
+           <br />
+          <img src={iconUrl(Apidata.data.weather[0].icon)} alt="" />
            </div>
            <div className="humidity mb-4">
            Humidity-{Apidata && Apidata.data.main.humidity}
